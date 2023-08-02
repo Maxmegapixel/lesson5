4740102de7513e08a3508d1e9083eaf3eb5b1b4f
Maksim <maxmegapixel@mail.ru>
2023-08-02 16:04:57 +0300

    задача 2 моё решение

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ int[] SumGet(int[] Massive)
     return Result;
 }    
 */
-// Задача 1: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+/*// Задача 1: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 int[] Array = ArrayGet(12, -9, 9);
 Console.WriteLine($"Массив начальный: [{String.Join(',', Array)}]");
 int[] Swap = SwapGet(Array);
@@ -62,4 +62,44 @@ int[] SwapGet(int[] Massive)
         Massive[i] *= -1;
     }
     return Massive;
+}
+*/
+// Задача 2. Присутствует ли заданное число в массиве
+int Number = 4;
+int[] Array = ArrayGet(5, 0, 9);
+Console.WriteLine($"Массив начальный: [{String.Join(',', Array)}]");
+int[] Value = FindGet(Array, Number);
+if (Value.Length == 0) Console.WriteLine("Таких чисел нет");
+else Console.WriteLine($"Индексы таких элементов [{String.Join(',', Value)}]");
+
+
+// Метод возвращает массив
+int[] ArrayGet(int Size, int MinVal, int MaxVal)
+{
+    int[] Result = new int[Size];
+    for (int i = 0; i < Size; i++)
+    {
+        Result[i] = new Random().Next(MinVal, MaxVal + 1);
+    }
+    return Result;
+}
+
+// Метод замены элементов массива
+int[] FindGet(int[] Massive, int Find)
+{
+    int count=0;
+    for (int i = 0; i < Massive.Length; i++)
+    {
+        count += Massive[i]==Find? 1 : 0;
+    }
+    int[] Position = new int[count];
+    int j = 0;
+    for (int i = 0; i < Massive.Length; i++)
+    {
+        if (Massive[i]==Find) 
+        {
+            Position[j++]=i;
+        }
+    }
+    return Position;
 }
