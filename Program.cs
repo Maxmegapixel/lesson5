493463fb79894251568cdab5df6e44bcc246f71c
Maksim <maxmegapixel@mail.ru>
2023-08-02 13:43:30 +0300

    task 1: array 12,-9,9; sum positiv/negativ

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+//Найдите сумму отрицательных и положительных элементов массива.
+int[] Array=ArrayGet(12,-9,9);
+Console.WriteLine($"Массив: [{String.Join(',',Array)}]");
+int[] Summ=SumGet(Array);
+Console.WriteLine($"Сумма положительных: {Summ[0]} \nСумма отрицательных: {Summ[1]}");
+
+
+// Метод возвращает массив
+
+int[] ArrayGet(int Size, int MinVal, int MaxVal)
+{
+    int[] Result = new int[Size];
+    for (int i = 0; i<Size; i++)
+    {
+        Result[i] = new Random().Next(MinVal, MaxVal+1);
+    }
+    return Result;
+}
+
+// Метод находит сумму элементов массива
+int[] SumGet(int[] Massive)
+{
+    int[] Result= new int[2];
+    int PositiveSum = 0;
+    int NegativeSum = 0;
+    foreach (int item in Massive)
+    {
+        PositiveSum += item > 0? item : 0;
+        NegativeSum += item < 0? item : 0;
+    }
+    Result[0] = PositiveSum;
+    Result[1] = NegativeSum;
+    return Result;
+}    
